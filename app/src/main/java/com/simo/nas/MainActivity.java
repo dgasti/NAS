b75f09db0e72332c9fc3f6734488a17e42e5a84a
dgasty <simo.gastaldo@gmail.com>
2016-05-25 19:12:48 +0200

    Initial commit

---
+++ app/src/main/java/com/simo/nas/MainActivity.java
@@ -0,0 +1,97 @@
+package com.simo.nas;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Properties;
+
+import com.jcraft.jsch.ChannelExec;
+import com.jcraft.jsch.JSch;
+import com.jcraft.jsch.Session;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+
+        String ok = "ok!";
+
+        try
+        {
+            JSch jsch = new JSch();
+            Session session = jsch.getSession("gasty","192.168.1.109", 51182);
+            session.setPassword("Zaffiro51");
+
+            // Avoid asking for key confirmation
+            Properties prop = new Properties();
+            prop.put("StrictHostKeyChecking", "no");
+            session.setConfig(prop);
+
+            session.connect();
+
+            // SSH Channel
+            ChannelExec channelssh = (ChannelExec) session.openChannel("exec");
+            //ByteArrayOutputStream baos = new ByteArrayOutputStream();
+           // channelssh.setOutputStream(baos);
+
+            // Execute command
+            channelssh.setCommand("sensors > /home/gasty/test.txt");
+
+            channelssh.connect();
+
+            channelssh.disconnect();
+
+            //System.out.println(baos.toString());
+
+        }
+        catch (Exception e)
+        {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
